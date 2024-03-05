9335f66709d4180e3a82dd559d6f3a4072792b78
Mykhailo_Beheka <Mykhailo_Beheka@epam.com>
2024-03-05 14:38:30 +0200

    Redeploy check

--- src/main/java/com/example/kubernetesclientsandbox/controller/HelloController.java
+++ src/main/java/com/example/kubernetesclientsandbox/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.kubernetesclientsandbox.controller;
 
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 @NoArgsConstructor
+@Slf4j
 public class HelloController {
 
     @GetMapping
     public String hello() {
+        log.info("Hello from kubernetes client sandbox!");
         return "Hello from kubernetes client sandbox!";
     }
 }
