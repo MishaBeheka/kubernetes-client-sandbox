19b70b6a5b616492e1d887287a64cab05104b56e
Mykhailo_Beheka <Mykhailo_Beheka@epam.com>
2024-03-05 11:39:03 +0200

    init commit

---
+++ src/main/java/com/example/kubernetesclientsandbox/KubernetesClientSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.kubernetesclientsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesClientSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesClientSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kubernetesclientsandbox/KubernetesClientSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kubernetesclientsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesClientSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
